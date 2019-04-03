980c961df16d963460c4ec7fc3b6af45e892020a
Zealll <eriznis@gmail.com>
2019-04-03 18:42:40 -0400

    GET by ID for COHORTS with a conditional added

--- data/helpers-endpoints/cohorts-router.js
+++ data/helpers-endpoints/cohorts-router.js
@@ -12,6 +12,23 @@ router.get('/', (req,res) => {
     .catch(error => res.status(500).json(error))
 })
 
+router.get('/:id', (req,res) => {
+    const id = req.params.id
+
+    cohorts
+    .getById(id)
+    .then(cohort => {
+        if(!cohort) {
+            res
+            .status(404)
+            .json({message: `Cohort with a specified ID of ${id} does not exist!`})
+        } else {
+            res.json(cohort)
+        }
+    })
+    .catch(error => res.status(500).json(error))
+})
+
 
 
 module.exports = router
